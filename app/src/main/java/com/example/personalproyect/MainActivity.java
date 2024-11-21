25663ba4875233670235a0eee056ded56c9c525c
unknown <ah5008034@gmail.com>
2024-11-21 12:41:20 -0600

    tacoMovil

--- app/src/main/java/com/example/personalproyect/MainActivity.java
+++ app/src/main/java/com/example/personalproyect/MainActivity.java
@@ -1,9 +1,7 @@
-package com.example.personal_project;
+  package com.example.personalproyect;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
+        image = findViewById(R.id.tacomovil);
+        image.setImageResource(R.drawable.tacomovil);
 
     }
-
 }
